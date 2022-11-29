f56f0b4b662a1f426d6a6f1c2d32d2b436c7b820
Gareema Ranjan <granjan@v1040-wn-rt-a-119-61.campus-dynamic.uwaterloo.ca>
2022-11-29 13:02:57 -0500

    added dec2bin-alt program

---
+++ new-benchmarks/decimal-to-binary-alt/r.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+set -o nounset
+set -o pipefail
+
+rm a.out temp.txt &> /dev/null
+
+readonly GCC="gcc"
+readonly CLANG="clang"
+#################################################################################
+
+### check for undefined behaviors first (from creduce scripts)
+
+rm -f out*.txt
+
+
+clang t.c -o out1
+./out1 > temp1-dec2bin.txt
+
+readonly EXIT_CODE="$?"
+echo $EXIT_CODE
+# cat temp1.txt
+if [[ "${EXIT_CODE}" == "0" ]] && grep -q "1010" temp1-dec2bin.txt ; then
+  exit 0
+  echo "will I be printed"
+fi
+
+# if grep -q "hello" temp1.txt ; then
+#   exit 0
+# fi
+
+#
+# compcert first
+#
+# now we have passed everything, return 0
+exit 1
