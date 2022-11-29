3db6dfb0acbf56d9e99f035740779c80560b04ba
Gareema Ranjan <granjan@v1040-wn-rt-a-119-61.campus-dynamic.uwaterloo.ca>
2022-11-29 17:50:27 -0500

    Added reverse string bm

---
+++ new-benchmarks/reverse/t.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(char a[], char b[])
+{
+    int flag = 0, i = 0;                 // integer variables declaration
+    while (a[i] != '\0' && b[i] != '\0') // while loop
+    {
+        if (a[i] != b[i])
+        {
+            flag = 1;
+            break;
+        }
+        i++;
+    }
+    if (flag == 0)
+        return 0;
+    else
+        return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("Enter a string!!\n");
+        return 0;
+    }
+    char *org = argv[1];
+    char *acData = argv[1], Temp = 0;
+    int iLoop = 0, iLen = 0;
+    while (acData[iLen++] != '\0')
+        ;
+    //Remove the null character
+    iLen--;
+
+    iLen--;
+    while (iLoop < iLen)
+    {
+        Temp = acData[iLoop];
+        acData[iLoop] = acData[iLen];
+        acData[iLen] = Temp;
+        iLoop++;
+        iLen--;
+    }
+
+    int res = compare(org, acData);
+    if (res == 0)
+    {
+        printf("Palindrome");
+    }
+    printf("\n\nReverse string is : %s\n\n", acData);
+    return 0;
+}
