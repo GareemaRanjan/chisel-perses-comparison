b2625cba6be26c11f603b84f310e0deb4e6f53f0
Gareema Ranjan <granjan@v1040-wn-rt-a-119-61.campus-dynamic.uwaterloo.ca>
2022-11-29 13:23:50 -0500

    added swap and sum

---
+++ new-benchmarks/swap_and_sum/perses.c
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+int main()
+{
+
+    int x[10], sum    ;
+    int i, n = 5;
+
+
+
+
+
+    for (     ; i <= n; i++)
+
+        x[i] = i;
+
+
+
+
+
+
+
+    for (i = 1; i <= n; i++)
+
+
+        sum +=   i ;
+
+
+    return sum;
+}
+
---
+++ new-benchmarks/swap_and_sum/r.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+set -o nounset
+set -o pipefail
+
+rm a.out temp.txt &> /dev/null
+
+readonly GCC="gcc"
+readonly CLANG="clang"
+#################################################################################
+
+### check for undefined behaviors first (from creduce scripts)
+
+rm -f out*.txt
+
+
+clang swap.c -o out1
+./out1 > temp1.txt
+
+readonly EXIT_CODE="$?"
+echo $EXIT_CODE
+# cat temp1.txt
+if [[ "${EXIT_CODE}" == "15" ]] ; then
+  exit 0
+  echo "will I be printed"
+fi
+
+# if grep -q "hello" temp1.txt ; then
+#   exit 0
+# fi
+
+#
+# compcert first
+#
+# now we have passed everything, return 0
+exit 1
